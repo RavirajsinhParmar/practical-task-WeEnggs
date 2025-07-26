eed0df866c2fe6d15a5dd450c59dd0c1d3c9b3a4
Ravirajsinh Parmar <parmarravirajsinhk@gmail.com>
2025-07-26 16:12:38 +0530

    Fetch json data path updated

--- src/services/estimateService.js
+++ src/services/estimateService.js
@@ -1,5 +1,7 @@
 export const fetchEstimateData = async () => {
-  const response = await fetch("/Estimate_detail.json");
+  const response = await fetch(
+    `${process.env.PUBLIC_URL}/Estimate_detail.json`
+  );
   const data = await response?.json();
   return data?.data?.sections || [];
 };
